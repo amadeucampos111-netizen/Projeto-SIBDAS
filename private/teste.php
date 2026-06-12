20c564dc62115a69cf5ae968d741565a4c70e084
Amadeu Martim Leal Campos <1240896@isep.ipp.pt>
2026-06-12 16:04:38 +0100

    Começo das validações para segurança do backend

--- private/teste.php
+++
@@ -1,9 +0,0 @@
-<?php
-$senha = 'PasseSegura123!';
-$novo_hash = password_hash($senha, PASSWORD_DEFAULT);
-
-echo "<h3>Gerador de Credenciais</h3>";
-echo "Palavra-passe: <strong>" . htmlspecialchars($senha) . "</strong><br>";
-echo "Novo Código Hash (Copia isto tudo):<br>";
-echo "<textarea rows='3' style='width:100%; font-family:monospace; margin-top:10px;' readonly>" . $novo_hash . "</textarea>";
-?>
